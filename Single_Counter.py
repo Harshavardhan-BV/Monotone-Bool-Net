8c5e2bcc853a9107c0438ce763155c51801820cf
Harshavardhan-BV <bharshavardh@iisc.ac.in>
2025-03-17 10:19:27 +0530

    Reorder functions to generate other single high states

--- Single_Counter.py
+++ Single_Counter.py
@@ -39,7 +39,23 @@ df_count = pd.DataFrame(index=findex, columns=['Freq'])
 for i in range(len(df_count)):
     f = df_count.index[i]
     df_count.iloc[i] = freq_index(df, f)
+#%%
+for i in range(df_count.index.nlevels):
+    # Reorder by shifting the order right
+    new_index = df_count.index.to_frame(index=False)
+    new_index = np.roll(new_index, i, axis=1)
+    new_index = pd.MultiIndex.from_arrays(new_index.T, names=df_count.index.names)
+    # Add a new columns where the orders are shifted
+    new_values = df_count['Freq'].reindex(new_index)
+    new_values.index = df_count.index
+    df_count[f'X{i}'] = new_values
+#%%
+# Drop the Freq column
+df_count.drop(columns='Freq', inplace=True)
+#%%
+# Sort by which has the highest sum
+df_count['Sum'] = df_count.sum(axis=1)
+df_count.sort_values(by='Sum', ascending=False, inplace=True)
 # %%
-df_count.sort_values(by='Freq', ascending=False, inplace=True)
 df_count.to_csv(f'Output/{n}-node_count.csv')
 # %%
