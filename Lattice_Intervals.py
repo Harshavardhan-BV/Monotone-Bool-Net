6bbc476b4ffdbb6f2cb4b15eb0b0f3bca8d25323
Harshavardhan-BV <bharshavardh@iisc.ac.in>
2025-07-10 11:54:51 +0530

    LL and UU intersections

--- Lattice_Intervals.py
+++ Lattice_Intervals.py
@@ -9,10 +9,12 @@ from common_func import read_IO
 from scipy.spatial.distance import pdist, squareform
 plt.rcParams['svg.hashsalt'] = ''
 os.makedirs('./figures/Intervals/',exist_ok=True)
+os.makedirs('./Output/Intervals/',exist_ok=True)
 #%%
-n = 2
+n = 4
 #%%
 df = read_IO(n+1)
+df = df.loc[df.sum(axis=1).sort_values().index]
 #%%
 U = np.argmax(df,axis=1)
 L = df.shape[1] - 1 - np.argmin(df.iloc[:, ::-1].values, axis=1)
@@ -37,16 +39,14 @@ for source, target_paths in nx.all_pairs_all_shortest_paths(G):
         all_nodes = set()
         for path in paths:
             all_nodes.update(path)
-        # all_nodes -= {source,target}
         intervals.at[source, target] = len(all_nodes)
 # %%
-def interval_hmap(intervals, pfx):
+def interval_hmap(intervals, pfx, xlabel='Target (L)', ylabel='Source (U)'):
     unique_vals = np.unique(intervals.values)
-    print(unique_vals)
     fig, ax = plt.subplots()
     sns.heatmap(intervals, cbar_kws={'ticks': unique_vals}, ax=ax,square=True)
-    ax.set_ylabel('Source (U)')
-    ax.set_xlabel('Target (L)')
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
     fig.tight_layout()
     plt.savefig(f'./figures/Intervals/{pfx}-intervals.svg')
 #%%
@@ -55,6 +55,33 @@ interval_hmap(intervals, pfx=f'B{n}-fn')
 LUinter = intervals.iloc[U,L]
 LUinter.columns = df.index
 LUinter.index = df.index
+LUinter.to_csv(f'./Output/Intervals/B{n}-LUinter.csv')
 # %%
 interval_hmap(LUinter,pfx=f'B{n}-input')
+# %%
+intervals = pd.DataFrame(0, index=G.nodes, columns=G.nodes, dtype=int)
+Lset = dict(nx.single_source_all_shortest_paths(G,df.columns[0]))
+for t1, path1 in Lset.items():
+    for t2, path2 in Lset.items():
+        intervals.at[t1,t2] = len(np.intersect1d(path1,path2))
+# %%
+LLinter = intervals.iloc[L,L]
+LLinter.columns = df.index
+LLinter.index = df.index
+LLinter.to_csv(f'./Output/Intervals/B{n}-LLinter.csv')
+# %%
+interval_hmap(LLinter,pfx=f'B{n}-LL', xlabel='L', ylabel='L')
+# %%
+intervals = pd.DataFrame(0, index=G.nodes, columns=G.nodes, dtype=int)
+Lset = dict(nx.single_source_all_shortest_paths(G.reverse(),df.columns[-1]))
+for t1, path1 in Lset.items():
+    for t2, path2 in Lset.items():
+        intervals.at[t1,t2] = len(np.intersect1d(path1,path2))
+# %%
+UUinter = intervals.iloc[U,U]
+UUinter.columns = df.index
+UUinter.index = df.index
+UUinter.to_csv(f'./Output/Intervals/B{n}-UUinter.csv')
+# %%
+interval_hmap(UUinter,pfx=f'B{n}-UU', xlabel='U', ylabel='U')
 # %%
